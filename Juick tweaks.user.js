0ecfae3fae7a846a704e8c7cd19c8be3b77cb835
MXI <KillyMXI@users.noreply.github.com>
2016-12-17 01:53:47 +0300

    1.2.2 (2016.09.03)
    Убрал изменение шрифта.

--- Juick tweaks.user.js
+++ Juick tweaks.user.js
@@ -4,7 +4,7 @@
 // @description Some feature testing
 // @match       *://juick.com/*
 // @author      Killy
-// @version     1.2.1
+// @version     1.2.2
 // @date        2.9.2016
 // @run-at      document-end
 // @grant none
@@ -126,15 +126,6 @@ function sortTags() {
   });
 }
 
-function changeFonts() {
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = 
-    //"@import 'https://fonts.googleapis.com/css?family=Open+Sans&subset=cyrillic,cyrillic-ext';" + 
-    "article > p, .msg-txt, input, textarea {font-family: -apple-system,BlinkMacSystemFont,Open Sans,Roboto,Oxygen,Droid Sans,Helvetica Neue,Verdana,sans-serif; font-size: 11pt;}";
-  document.body.appendChild(css);
-}
-
 var isPost = document.getElementById("content").hasAttribute("data-mid");
 var isFeed = (document.getElementById("content").getElementsByTagName('article').length > 1);
 var isPostEditorSharp = (document.getElementById('newmessage') === null) ? false : true;
@@ -157,4 +148,3 @@ if(isPostEditorSharp) {
 if(isTagsPage) {
   sortTags();
 }
-changeFonts();
