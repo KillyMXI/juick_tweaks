60c3886edb7466f73b8b7bb6ce98cf968c98cbde
Killy <killy.mxi@gmail.com>
2018-05-21 21:34:27 +0300

    update regex to include /m links

--- juick_tweaks.user.js
+++ juick_tweaks.user.js
@@ -1265,7 +1265,7 @@ function getEmbeddableLinkTypes() {
       className: 'juickEmbed',
       onByDefault: true,
       ctsDefault: false,
-      re: /^(?:https?:)?\/\/juick\.com\/(?!tag\/)(?:([\w-]+)\/)?([\d]{6,}\b)(?:#(\d+))?/i,
+      re: /^(?:https?:)?\/\/juick\.com\/(?!tag\/)(?:m\/|([\w-]+)\/|)([\d]{6,}\b)(?:#(\d+))?/i,
       ctsMatch: function(aNode, reResult) {
         let [url, userId, msgId, replyId] = reResult;
         let thisPageMsgMatch = /\/(\d+)$/.exec(window.location.pathname);
